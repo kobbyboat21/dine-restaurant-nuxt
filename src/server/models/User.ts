438dff98ed60bb0c420a7509dec3437f993e8cc4
Senku <senku@reddune.dev>
2024-06-05 18:47:40 +0200

    [feature]: Authentication - MongoDB User Schema
    Issue #1
    
    Created the User Schema for Mongoose to be used for Authentication, with
    hashing leveraging bcrypt

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -1,11 +1,28 @@
 import { Schema, model } from 'mongoose'
 import bcrypt from "mongoose-bcrypt";
 
-const UserSchema = new Schema ({
-  id: String,
-  username: String,
-  email: String,
+const UserSchema =  new Schema({
+  _id: { type: String, required: true, unique: true }, // Use a unique string as the id
+  username: { type: String, required: true, unique: true }, // Unique username
+  email: { type: String, required: true, unique: true }, // Unique email
+  password: { type: String, required: true, bcrypt: true } // Plain text password (will be hashed before saving)
+});
 
-})
+
+UserSchema.plugin(bcrypt);
+/* VERIFY PASSWORDS WITH:
+ // Verify password with callback
+user.verifyPassword('plaintextPassword', function(err, isMatch) {
+  if (err) return handleError(err);
+  // isMatch will be true if the password matches
+});
+
+// Verify password with Promise
+user.verifyPassword('plaintextPassword')
+  .then(isMatch => {
+    // isMatch will be true if the password matches
+  })
+  .catch(err => handleError(err));
+*/
 
 export default model("User", UserSchema);
